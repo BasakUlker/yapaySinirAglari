3c3ba8bf84a5c6617d39c41f4736120b9f15b0e6
BasakUlker <56607821+BasakUlker@users.noreply.github.com>
2023-04-13 23:22:34 +0300

    Create soru4_1.py

---
+++ soru4_1.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+torch.manual_seed(1)
+X = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float)
+
+class myClass(nn.Module):
+
+    def __init__(self):
+
+        super(myClass, self).__init__()
+        self.hidden = nn.Linear(3, 50)
+        self.output = nn.Linear(50, 1)
+
+    def forward(self, x):
+
+        x = torch.tanh(self.hidden(x))
+        x = torch.sigmoid(self.output(x))
+        return x
+
+model = myClass()
+output = model(X)
+
+print(output)
